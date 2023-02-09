2b1b9bec223d9acf89bd7e9d5b28e2fb83f67cd9
saad-69 <guXEbZG33xiYR3k>
2023-02-09 16:26:43 +0300

    Profile: Connect to the database

--- app/src/main/java/com/aos/seed/Ui/Profile.java
+++ app/src/main/java/com/aos/seed/Ui/Profile.java
@@ -6,14 +6,33 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.aos.seed.Model.StoreTopView;
 import com.aos.seed.R;
 import com.aos.seed.databinding.FragmentProfileBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.concurrent.Executor;
 
 public class Profile extends Fragment {
 
     FragmentProfileBinding binding;
+    private FirebaseAuth auth;
+    private FirebaseFirestore mDb ;
+    private String uid;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -21,11 +40,52 @@ public class Profile extends Fragment {
 
 
 
+        auth = FirebaseAuth.getInstance();
+        mDb = FirebaseFirestore.getInstance();
+        uid = auth.getCurrentUser().getUid();
+
+        String name = binding.Fname.getText().toString();
+        String phone = binding.Mobile.getText().toString();
+
+
+         mDb.collection("Customer").document(uid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+             @Override
+             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                 if (task.isSuccessful()) {
+                     DocumentSnapshot snapshots = task.getResult();
+
+                     binding.Fname.setText(snapshots.getString("Name"));
+                     binding.em.setText(snapshots.getString("email"));
+                     binding.Mobile.setText(snapshots.getString("phone"));
+
+
+                 }
+
+             }
+         });
+
+
+
+
         binding.saveData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getFragmentManager().beginTransaction().replace(R.id.frameLayout, new Store()).commit();
-            }
+                if (name.isEmpty()){
+                    binding.Fname.setError("Full Name");
+                    binding.Fname.setFocusable(true);
+                }else if(phone.isEmpty()){
+                    binding.Mobile.setError("Mobile");
+                    binding.Mobile.setFocusable(true);
+                }else {
+                    mDb.collection("Customer").document(uid).update("Name",binding.Fname.getText().toString());
+                    mDb.collection("Customer").document(uid).update("phone",binding.Mobile.getText().toString());
+                    getFragmentManager().beginTransaction().replace(R.id.frameLayout, new Store()).commit();
+                }
+
+
+
+
+                }
         });
 
 
